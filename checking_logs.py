dd0eac91562c2a163c332da83ef71be4468432cf
mister-utka <warashovandry@gmail.com>
2024-01-10 21:47:57 +0700

    Logutka_0.0.2

--- checking_logs.py
+++ checking_logs.py
@@ -1,21 +1,20 @@
 import subprocess
 from subprocess import call, check_output, run
+from pprint import pprint
+
 
 def checking_logs(working_directory):
-    """Функция сканирует логи на предмет указанных в grep_cmd_ist"""
+    """Функция сканирует логи на предмет указанных в grep_cmd_list"""
 
     def log_reading(file):
         """Считывание существующего лога и создание из него списка для сравнения"""
         with open(file, "r") as f:
             reading_log = [line.strip() for line in f]
-            # if reading_log == []:
-            #     reading_log.append("")
         return reading_log
 
     def log_entry_a(output, file):
         """Запись построчно полученных данных в файл логов"""
         with open(file, "a") as f:
-            #output_log = []
             if output is None:
                 pass
             else:
@@ -27,27 +26,35 @@ def checking_logs(working_directory):
         return list(diff)
 
     def checking_logs(grep_cmd_list):
+        """Функция считывает уже существующие логи из файла
+           После происходит сравнение с полученными логами
+           Отличия дозаписываются в файл"""
+
+        # Запишем текущие логи из файла
         output_list = []
+        reading_log = log_reading(file)
 
         # Перебор элементов в списке
         for grep_cmd in grep_cmd_list:
 
-            output = run(f'cat {working_directory}../*.* | grep {grep_cmd}', shell=True, stdout=subprocess.PIPE,
-                         text=True).stdout
-            # print(output)
+            output = run(f'cat {working_directory}../*.* | grep {grep_cmd}',
+                         shell=True, stdout=subprocess.PIPE, text=True).stdout
+
+            # Получаем список из логов
             output = output.strip().split("\n")
+            # Так как команд может быть несколько, объеденим вывод в один список
             if output != ['']:
                 output_list = output_list + output
 
-        reading_log = log_reading(file)
-
+        # Получим различия между списками
         differences = compare_lists(output_list, reading_log)
-        # print("Различия между списками:", differences)
+
+        # Если есть различия, запишем их в файл
         if differences == []:
             pass
-            # print("1 Сходится!")
+            # print("Сходится!")
         else:
-            # print("1 Не сходится!")
+            # print("Не сходится!")
             for diff in differences:
                 log_entry_a(diff + "\n", file)
 
@@ -67,18 +74,13 @@ def checking_logs(working_directory):
     for key, grep_cmd_list in grep_cmd_dict.items():
 
         if key == 'port_scan_logs':
-
             file = f"{working_directory}port_scan_logs_file"
             checking_logs(grep_cmd_list)
 
-
         if key == 'failed_authentication_logs':
-
             file = f"{working_directory}failed_authentication_logs_file"
             checking_logs(grep_cmd_list)
 
-
         if key == 'failed_authentication_ssh_logs':
-
             file = f"{working_directory}failed_authentication_ssh_logs_file"
             checking_logs(grep_cmd_list)
